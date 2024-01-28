894a5f1c58bf803bb2796258c38170db89f4783f
Xinying Chew <x.chew3@gmail.com>
2024-01-27 21:06:48 -0500

    Update json and checkboxes

--- public/main.js
+++ public/main.js
@@ -13,16 +13,13 @@ let check2 = [false, false, false]
 let check3 = [false, false, false]
 let check4 = [false, false, false]
 
-document.getElementById('helpButton').addEventListener('click', function () {
 
-    // const problem = document.getElementById('problem').value;
-    // const track = document.getElementById('track').value;
-    // const field = document.getElementById('field').value;
+document.getElementById('helpButton').addEventListener('click', function () {
 
     const teamNumber = localStorage.getItem('teamNum');
     const progress = document.querySelector('input[name="progress"]:checked').getAttribute("value");
     const track = document.querySelector('input[name="track"]:checked').getAttribute("value");
-    const hardware = getCheckedCheckboxes();
+    const hardware = document.querySelector('input[name="hardware"]:checked').getAttribute("value");
     const problem = document.getElementById('problemDescription').value;
     const field = document.querySelector('input[name="request"]:checked').getAttribute("value");;
 
@@ -32,17 +29,8 @@ document.getElementById('helpButton').addEventListener('click', function () {
     console.log(message);
     // Send the message as JSON to the WebSocket server
     ws.send(JSON.stringify(message));
-
-    getCheckedCheckboxes()
-
-    localStorage.setItem('check1', check1);
-    localStorage.setItem('check2', check2);
-    localStorage.setItem('check3', check3);
-    localStorage.setItem('check4', check4);
-
+    console.log(JSON.stringify(message));
 });
-
-// var checklistContainer = document.getElementById('progress-checklist');
 
 function saveCheckBox(boxNum) {
     if (curProg == Status.PROBLEM) {
@@ -53,6 +41,7 @@ function saveCheckBox(boxNum) {
             document.getElementById("img1").style.display = 'none';
         }
         // document.getElementById(`box${boxNum}`).checked = true;
+        localStorage.setItem('check1', check1);
     } else if (curProg == Status.IDEATION) {
         check2[boxNum - 1] = !check2[boxNum - 1];
         if (check2.every(value => value === true)) {
@@ -60,6 +49,7 @@ function saveCheckBox(boxNum) {
         } else {
             document.getElementById("img2").style.display = 'none';
         }
+        localStorage.setItem('check2', check2);
     } else if (curProg == Status.PROTOTYPING) {
         check3[boxNum - 1] = !check3[boxNum - 1];
         if (check3.every(value => value === true)) {
@@ -67,6 +57,7 @@ function saveCheckBox(boxNum) {
         } else {
             document.getElementById("img3").style.display = 'none';
         }
+        localStorage.setItem('check3', check3);
     } else if (curProg == Status.PRESENTATION) {
         check4[boxNum - 1] = !check4[boxNum - 1];
         if (check4.every(value => value === true)) {
@@ -74,6 +65,7 @@ function saveCheckBox(boxNum) {
         } else {
             document.getElementById("img4").style.display = 'none';
         }
+        localStorage.setItem('check4', check4);
     }
 }
 
@@ -81,31 +73,31 @@ function updateChecklistOptions(selectedOption) {
 
  // Add new checklist options based on the selected radio button
     if (selectedOption === 'Problem') {
-        document.getElementById('prog-prob').style.display = 'block'
-        document.getElementById('prog-idea').style.display = 'none'
-        document.getElementById('prog-proto').style.display = 'none'
-        document.getElementById('prog-presen').style.display = 'none'
+        document.getElementById('op1').style.display = 'block'
+        document.getElementById('op2').style.display = 'none'
+        document.getElementById('op3').style.display = 'none'
+        document.getElementById('op4').style.display = 'none'
         curProg = Status.PROBLEM;
     
     } else if (selectedOption === 'Ideation') {
-        document.getElementById('prog-prob').style.display = 'none'
-        document.getElementById('prog-idea').style.display = 'block'
-        document.getElementById('prog-proto').style.display = 'none'
-        document.getElementById('prog-presen').style.display = 'none'
+        document.getElementById('op1').style.display = 'none'
+        document.getElementById('op2').style.display = 'block'
+        document.getElementById('op3').style.display = 'none'
+        document.getElementById('op4').style.display = 'none'
         curProg = Status.IDEATION;
         
     } else if (selectedOption === 'Prototyping') {
-        document.getElementById('prog-prob').style.display = 'none'
-        document.getElementById('prog-idea').style.display = 'none'
-        document.getElementById('prog-proto').style.display = 'block'
-        document.getElementById('prog-presen').style.display = 'none'
+        document.getElementById('op1').style.display = 'none'
+        document.getElementById('op2').style.display = 'none'
+        document.getElementById('op3').style.display = 'block'
+        document.getElementById('op4').style.display = 'none'
         curProg = Status.PROTOTYPING;
         
     } else if (selectedOption === 'Presentation') {
-        document.getElementById('prog-prob').style.display = 'none'
-        document.getElementById('prog-idea').style.display = 'none'
-        document.getElementById('prog-proto').style.display = 'none'
-        document.getElementById('prog-presen').style.display = 'block'
+        document.getElementById('op1').style.display = 'none'
+        document.getElementById('op2').style.display = 'none'
+        document.getElementById('op3').style.display = 'none'
+        document.getElementById('op4').style.display = 'block'
         curProg = Status.PRESENTATION;
     }
 }
@@ -128,13 +120,14 @@ document.getElementById('signInAgain').addEventListener('click', function () {
 
 });
 
-function checkLogin() {
-    // console.log(localStorage.getItem('teamNum'));
+function loadMain() {
+    // load numLikes
     if (localStorage.getItem('numLikes') == null) {
         localStorage.setItem("numLikes", 0);
     }
-
     document.getElementById('likes').innerHTML = 'likes: ' + localStorage.getItem('numLikes');
+
+    // load teamNum
     if (localStorage.getItem('teamNum') == null){
         document.getElementById('loginDiv').style.display = 'block';
         document.getElementById('mainDiv').style.display = 'none';
@@ -143,37 +136,75 @@ function checkLogin() {
         document.getElementById('mainDiv').style.display = 'block';
         document.getElementById('teamNumTitle').innerHTML = `Team Number ${localStorage.getItem('teamNum')}`;
     }
+
+    // load checklist
+    if (localStorage.getItem('check1') != null) {
+        check1 = localStorage.getItem('check1').split(',').map(value => value === 'true');;
+        console.log(check1)
+    }
+    if (localStorage.getItem('check2') != null) {
+        check2 = localStorage.getItem('check2').split(',').map(value => value === 'true');;
+    }
+    if (localStorage.getItem('check3') != null) {
+        check3 = localStorage.getItem('check3').split(',').map(value => value === 'true');;
+    }
+    if (localStorage.getItem('check4') != null) {
+        check4 = localStorage.getItem('check4').split(',').map(value => value === 'true');;
+    }
+
+    if (check1.every(value => value === true)) {
+        document.getElementById("img1").style.display = 'block';
+    } 
+    if (check2.every(value => value === true)) {
+        document.getElementById("img2").style.display = 'block';
+    }
+    if (check3.every(value => value === true)) {
+        document.getElementById("img3").style.display = 'block';
+    } 
+    if (check4.every(value => value === true)) {
+        document.getElementById("img4").style.display = 'block';
+    }
+
+    updateCheckboxes(check1, 'op1');
+    updateCheckboxes(check2, 'op2');
+    updateCheckboxes(check3, 'op3');
+    updateCheckboxes(check4, 'op4');
 }
 
-function getCheckedCheckboxes() {
-    const checkboxes = document.getElementById('hardwareDiv').querySelectorAll('input[type="checkbox"]');
-    const checkedCheckboxes = Array.from(checkboxes).filter(checkbox => checkbox.checked);
-    const checkedValues = checkedCheckboxes.map(checkbox => checkbox.value);
-    return checkedValues;
-  }
+function updateCheckboxes(array, group) {
+    console.log("checking group: " + group);
+    const checkboxes = document.getElementById(group).querySelectorAll(`input[type="checkbox"]`);
+    // const checkboxes = document.querySelectorAll(`.${group} input[type="checkbox"]`);
+    console.log(array);
+    checkboxes.forEach((checkbox, index) => {
+        console.log(array[index], index);
+        checkbox.checked = (array[index]);
+    });
+}
+
+// function getCheckedCheckboxes() {
+//     const checkboxes = document.getElementById('hardwareDiv').querySelectorAll('input[type="checkbox"]');
+//     const checkedCheckboxes = Array.from(checkboxes).filter(checkbox => checkbox.checked);
+//     const checkedValues = checkedCheckboxes.map(checkbox => checkbox.value);
+//     return checkedValues;
+//   }
 
 // change to your ip address
 const ws = new WebSocket('ws://10.29.183.155:8082');
 const form = document.querySelector('form')
 
 ws.onmessage = (e) => {
-    const bufferData = e.data;
-    // console.log(typeof bufferData);
-    
-    // console.log(JSON.parse(bufferData));
-
-    // const dataArray = JSON.parse(bufferData).data;
-
-    // // Convert numeric values to ASCII characters
-    // const asciiString = String.fromCharCode(...dataArray);
-
-    // // Parse the resulting string as JSON
-    // const jsonData = JSON.parse(asciiString);
-    // // console.log(jsonData.teamNumber);
-    // // console.log("hi");
-
-    // if (localStorage.getItem('teamNum') != null && localStorage.getItem('teamNum') == jsonData.teamNumber) {
-    //    localStorage.setItem("numLikes", parseInt(localStorage.getItem("numLikes")) + 1);
-    //    document.getElementById('likes').innerHTML = 'likes: ' + localStorage.getItem('numLikes');
-    // }
+    const blobData = e.data;
+    const reader = new FileReader();
+    reader.onload = function() {
+        const textData = reader.result;
+        const jsonData = JSON.parse(textData);
+        if (jsonData['likedTeamNum'] != undefined) {
+            if (localStorage.getItem('teamNum') != null && localStorage.getItem('teamNum') == jsonData['likedTeamNum']) {
+                localStorage.setItem("numLikes", parseInt(localStorage.getItem("numLikes")) + 1);
+                document.getElementById('likes').innerHTML = 'likes: ' + localStorage.getItem('numLikes');
+            }
+        }
+    };
+    reader.readAsText(blobData);
 };
--- server/app.js
+++ server/app.js
@@ -19,21 +19,14 @@ wss.on("connection", (ws) => {
   console.log("websocket connection established");
 
   ws.on('message', (e) => {
+    // console.log(e);
     console.log(JSON.parse(e));
-    if (JSON.parse(e)['teamNumber'] != undefined) {
-
-      console.log(JSON.parse(e)['teamNumber']);
-      // if (localStorage.getItem('teamNum') != null && localStorage.getItem('teamNum') == JSON.parse(e)['teamNumber']) {
-      //   localStorage.setItem("numLikes", localStorage.getItem("numLikes") + 1);
-      //   document.getElementById('likes').innerHTML = 'likes: ' + localStorage.getItem('numLikes');
-      // }
-    }
-
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(e);
       }
     })
+    
   });
 
   ws.on("close", () => {
